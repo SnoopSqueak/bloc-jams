b3bf6989fb18c49b2bba4731ff93cd1848ebffaa
Mike Stowe <mstowe@mcglab.local>
2017-01-14 11:58:07 -0600

    Add play button that appears when mousing over a song name and disappears and mouse leaves

--- scripts/album.js
+++ scripts/album.js
@@ -54,7 +54,7 @@ var currentAlbum = 0;
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -83,9 +83,26 @@ var setCurrentAlbum = function(albumIndex) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- 
+
+ var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+ var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>'; 
+
  window.onload = function() {
      setCurrentAlbum(0);
+     songListContainer.addEventListener('mouseover', function(event) {
+         // Only target individual song rows during event delegation
+         if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+     
+    for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
  };
 
 document.getElementsByClassName('album-cover-art')[0].onclick = function () {
